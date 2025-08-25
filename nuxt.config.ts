f4eae131b7c38fb89dadb14a4e28dcc9a3288cd2
chenyingxu <chenyingxu@zingfront.com>
2025-08-25 15:47:26 +0800

    fix   gitignore

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 export default defineNuxtConfig({
-  css: ["~/assets/css/main.css"],
+  css: ["~/assets/css/main.css","@/assets/css/tailwind.css"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
   plugins: ["~/plugins/element-plus", "~/plugins/pinia-persistedstate"],
   modules: ["@pinia/nuxt"],
   runtimeConfig: {
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./components/**/*.{vue,js}", "./layout/**/*.vue", "./pages/**/*.vue", "./app.vue"],
+  theme: {
+    extend: {
+      keyframes: {
+        "fade-in-up": {
+          "0%": { opacity: "0", transform: "translateY(20px)" },
+          "100%": { opacity: "1", transform: "translateY(0)" },
+        },
+      },
+      animation: {
+        "fade-in-up": "fade-in-up 0.8s ease-out forwards",
+      },
+    },
+  },
+  plugins: [],
+};
